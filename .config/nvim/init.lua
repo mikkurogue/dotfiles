1eeba9008e71d428c29e39af5b9e8c6176aef7d3
mikkurogue <michael.lindemans@outlook.com>
2026-02-02 17:56:39 +0200

    back to one dark cause atom/zed did it right ngl

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -193,7 +193,7 @@ local schemes = {
 
 
 -- set colorscheme
-v.cmd("colorscheme " .. schemes[7])
+v.cmd("colorscheme " .. schemes[3])
 
 require("configuration.todo-comments")
 require("configuration.mini")
