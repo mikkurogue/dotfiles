70e834cfc2197063310047a5b6efa64ddd99ee6a
mikkurogue <michael.lindemans@outlook.com>
2026-02-18 12:02:37 +0200

    some stuff changed

--- .config/nvim/lua/configuration/fff.lua
+++ .config/nvim/lua/configuration/fff.lua
@@ -1,12 +1,22 @@
-require("fff").setup()
+-- Create a visible cursor highlight for the selected row
+-- (CursorLine may not be visible in some colorschemes like onedark)
+vim.api.nvim_set_hl(0, 'FFFCursor', { link = 'Visual' })
 
-vim.g.fff = {
+-- Config must be passed directly to setup()
+require("fff").setup({
   lazy_sync = false,
+  prompt = '  ',
+  layout = {
+    prompt_position = 'top',
+  },
+  hl = {
+    cursor = 'FFFCursor',
+  },
   git = {
-    status_text_color = true
+    status_text_color = true,
   },
   debug = {
     enabled = false,
     show_scores = false,
-  }
-}
+  },
+})
