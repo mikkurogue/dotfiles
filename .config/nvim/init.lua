de1e1303530e04866d9826a3795f0fc8168b0106
mikkurogue <michael.lindemans@outlook.com>
2026-03-22 19:38:54 +0200

    add nvim ufo and fix some of the folding stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -38,6 +38,8 @@ v.pack.add({
   { src = "https://github.com/saghen/blink.pairs" },
   { src = "https://github.com/vuki656/package-info.nvim" },
   { src = "https://github.com/goolord/alpha-nvim" },
+  { src = "https://github.com/kevinhwang91/promise-async" },
+  { src = "https://github.com/kevinhwang91/nvim-ufo" },
   -- Debugging (DAP)
   { src = "https://github.com/mfussenegger/nvim-dap" },
   { src = "https://github.com/rcarriga/nvim-dap-ui" },
@@ -60,6 +62,7 @@ v.pack.add({
 require("configuration.fff")
 
 require("package-info").setup()
+require("configuration.ufo")
 
 v.api.nvim_create_autocmd("PackChanged", {
   callback = function(ev)
